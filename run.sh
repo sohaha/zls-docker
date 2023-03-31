5d3c0697a87ea5e68e82837f33f847563922da53
影浅 <soyz@qq.com>
2023-03-31 13:48:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -207,6 +207,7 @@ function _installDocker() {
     tips 'command:'
     tips "        sudo curl -sSL https://get.docker.com | sh"
     tips "        sudo usermod -aG docker $USER"
+    tips "        newgrp docker"
     tips "        sudo curl -L https://get.daocloud.io/docker/compose/releases/download/v2.16.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose"
     tips "        sudo chmod +x /usr/local/bin/docker-compose"
     tips "        docker-compose --version"
@@ -218,6 +219,7 @@ function _installDocker() {
     tips 'command:'
     tips "        sudo curl -sSL https://get.docker.com | sh"
     tips "        sudo usermod -aG docker $USER"
+    tips "        newgrp docker"
     tips "        sudo curl -L https://get.daocloud.io/docker/compose/releases/download/v2.16.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose"
     tips "        sudo chmod +x /usr/local/bin/docker-compose"
     tips "        docker-compose --version"
